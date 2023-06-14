14ae418de9242d4833fc59e801bbda200811ef85
TDmitrii78 <hdmail2022@gmail.com>
2023-06-14 21:49:43 +0300

    final add animation

--- src/js/script.js
+++ src/js/script.js
@@ -63,18 +63,14 @@ function slider() {
 }
 slider();
 
-function topFixedMenu() {
-    const nav = document.querySelector('.header__nav');
-    window.onscroll = () => {
-        if (window.pageYOffset >= 1000) {
-            nav.classList.add('header__nav_menu-top-fixed-active')
-        } else {
-            nav.classList.remove('header__nav_menu-top-fixed-active')
-        }
+const back = document.querySelector('.back');
+function backToTop() {
+    if (window.pageYOffset >= 1000) {
+        back.classList.add('back_active');
+    } else {
+        back.classList.remove('back_active');
     }
-
 }
-topFixedMenu();
 
 function menu() {
     const hamburger = document.querySelector('.hamburger');
@@ -85,3 +81,129 @@ function menu() {
     }
 }
 menu();
+
+function animationElement(settingArrey) {
+    class Element {
+        constructor(track, animateElement, activClass, disableClass, enableClass, startDelay, delayBetweenElement, ratio, sensivityOffset, cycleAnimation) {
+            this.track = track,
+            this.animateElement = animateElement,
+            this.activClass = activClass,
+            this.disableClass = disableClass,
+            this.enableClass = enableClass,
+            this.startDelay = startDelay,
+            this.delayBetweenElement = delayBetweenElement,
+            this.ratio = ratio,
+            this.cycleAnimation = cycleAnimation,
+            this.sensivityOffset = sensivityOffset,
+            this.visibleAnimate = false,
+            this.done = false
+        }
+        detector() {
+            const track = (this.track) ? this.track: this.animateElement;
+            if ((track.offsetTop - window.innerHeight + this.sensivityOffset <= window.pageYOffset * this.ratio)) {
+                this.visibleAnimate = true;
+            }
+            if ((track.offsetTop - window.innerHeight - this.sensivityOffset > window.pageYOffset * this.ratio)) {
+                this.visibleAnimate = false;
+            }
+        }
+    }
+    
+    function createElement(trackElement, animateElements, activClass, disableClass, enableClass, startDelay, delayBetweenElement, ratio, sensivityOffset, cycleAnimation) {
+        const track = (trackElement) ? document.querySelector(`${(trackElement)}`) : false; 
+        const allAnimateElement = document.querySelectorAll(`${animateElements}`);
+        const arr = [];
+        for (el of allAnimateElement) {
+            arr.push(new Element(track, el, activClass, disableClass, enableClass, startDelay, delayBetweenElement, ratio, sensivityOffset, cycleAnimation));
+        }
+        startupEnableClass(arr, enableClass);
+        startupDisableClass(arr, disableClass);
+        return arr;
+    }
+
+    const arrObjElement = [];
+
+    for (item of settingArrey) {
+        arrObjElement.push(createElement(...item));
+    }
+
+    function  startupDisableClass(arrObjElement, disableClass) {
+        if (disableClass) {
+            removeClass(arrObjElement, disableClass);
+        }
+    }
+
+    function removeClass(arrObjElement, disableClass) {
+        for (item of disableClass.split(' ')) {
+            arrObjElement.forEach(el => {
+                if (el.animateElement.classList.contains(item)) {
+                    el.animateElement.classList.remove(item);
+                }
+            })
+        }  
+    }
+
+    function  startupEnableClass(arrObjElement, enableClass) {
+        if (enableClass) {
+            addClass(arrObjElement, enableClass);
+        }
+    }
+
+    function addClass(arrObjElement, enableClass) {
+        for (item of enableClass.split(' ')) {
+            arrObjElement.forEach(el => { 
+                if (!el.animateElement.classList.contains(item)) {
+                    el.animateElement.classList.add(item);
+                }
+            })
+        } 
+    }
+   
+    function baseLogic(arrObjElement) {
+        arrObjElement.forEach((item, index, arr) => {
+            item.detector();
+            if (item.visibleAnimate) {
+                if (!item.animateElement.classList.contains(item.activClass) & !item.done) {
+                    item.done = true;
+                    setTimeout(() => {
+                        item.animateElement.classList.add(`${item.activClass}`);
+                    }, item.startDelay + delay);
+                    console.log(delay);
+                    delay += item.delayBetweenElement;
+                    if (arr.length <= index + 1) {
+                        delay = 0;
+                    }
+         
+                }
+            }
+            if (!item.visibleAnimate & item.cycleAnimation) {
+                if (item.animateElement.classList.contains(item.activClass)) {
+                    item.done = false;
+                    delay = 0;
+                    item.animateElement.classList.remove(`${item.activClass}`);
+                }
+            }
+        })
+    }
+
+    let delay = 0;
+    
+    return function start() {
+        for (item of arrObjElement) {
+            baseLogic(item);
+        }
+    }
+}
+const animation = animationElement([
+    [false, ".advantages__item", "animation", '', 'advantages__item_translate-left', 0, 0, 1, 0, true],
+    [false, ".work__item", "work__item_animation", '', '', 0, 500, 0.9, 0, true],
+    [false, ".prices__item", "prices__item_animation", '', '', 0, 200, 1, 0, true],
+    [false, ".scheme__item", "scheme__item_animation", '', '', 0, 0, 1, 0, true]
+]);
+
+window.onscroll = () => {
+    animation();
+    backToTop();
+}
+
+
